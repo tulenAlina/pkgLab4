9fc03bd57a3b93bdbe3b65767a2c7485879d90f7
Alina Yashenok <AlinaYashenok@gmail.com>
2023-11-27 11:58:03 +0300

    Add project

---
+++ Executable/moc_mainwindow.cpp
@@ -0,0 +1,292 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Lab 4/mainwindow.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "on_doubleSpinBox_editingFinished",
+    "",
+    "on_doubleSpinBox_2_editingFinished",
+    "on_doubleSpinBox_3_editingFinished",
+    "on_doubleSpinBox_4_editingFinished",
+    "on_comboBox_currentIndexChanged",
+    "index",
+    "on_horizontalSlider_sliderMoved",
+    "position",
+    "on_horizontalSlider_3_sliderMoved",
+    "on_horizontalSlider_4_valueChanged",
+    "value",
+    "on_horizontalSlider_5_valueChanged",
+    "on_horizontalSlider_rangeChanged",
+    "min",
+    "max",
+    "on_horizontalSlider_valueChanged",
+    "on_horizontalSlider_3_valueChanged",
+    "on_pushButton_clicked",
+    "on_pushButton_2_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[42];
+    char stringdata0[11];
+    char stringdata1[33];
+    char stringdata2[1];
+    char stringdata3[35];
+    char stringdata4[35];
+    char stringdata5[35];
+    char stringdata6[32];
+    char stringdata7[6];
+    char stringdata8[32];
+    char stringdata9[9];
+    char stringdata10[34];
+    char stringdata11[35];
+    char stringdata12[6];
+    char stringdata13[35];
+    char stringdata14[33];
+    char stringdata15[4];
+    char stringdata16[4];
+    char stringdata17[33];
+    char stringdata18[35];
+    char stringdata19[22];
+    char stringdata20[24];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 32),  // "on_doubleSpinBox_editingFinished"
+        QT_MOC_LITERAL(44, 0),  // ""
+        QT_MOC_LITERAL(45, 34),  // "on_doubleSpinBox_2_editingFin..."
+        QT_MOC_LITERAL(80, 34),  // "on_doubleSpinBox_3_editingFin..."
+        QT_MOC_LITERAL(115, 34),  // "on_doubleSpinBox_4_editingFin..."
+        QT_MOC_LITERAL(150, 31),  // "on_comboBox_currentIndexChanged"
+        QT_MOC_LITERAL(182, 5),  // "index"
+        QT_MOC_LITERAL(188, 31),  // "on_horizontalSlider_sliderMoved"
+        QT_MOC_LITERAL(220, 8),  // "position"
+        QT_MOC_LITERAL(229, 33),  // "on_horizontalSlider_3_sliderM..."
+        QT_MOC_LITERAL(263, 34),  // "on_horizontalSlider_4_valueCh..."
+        QT_MOC_LITERAL(298, 5),  // "value"
+        QT_MOC_LITERAL(304, 34),  // "on_horizontalSlider_5_valueCh..."
+        QT_MOC_LITERAL(339, 32),  // "on_horizontalSlider_rangeChanged"
+        QT_MOC_LITERAL(372, 3),  // "min"
+        QT_MOC_LITERAL(376, 3),  // "max"
+        QT_MOC_LITERAL(380, 32),  // "on_horizontalSlider_valueChanged"
+        QT_MOC_LITERAL(413, 34),  // "on_horizontalSlider_3_valueCh..."
+        QT_MOC_LITERAL(448, 21),  // "on_pushButton_clicked"
+        QT_MOC_LITERAL(470, 23)   // "on_pushButton_2_clicked"
+    },
+    "MainWindow",
+    "on_doubleSpinBox_editingFinished",
+    "",
+    "on_doubleSpinBox_2_editingFinished",
+    "on_doubleSpinBox_3_editingFinished",
+    "on_doubleSpinBox_4_editingFinished",
+    "on_comboBox_currentIndexChanged",
+    "index",
+    "on_horizontalSlider_sliderMoved",
+    "position",
+    "on_horizontalSlider_3_sliderMoved",
+    "on_horizontalSlider_4_valueChanged",
+    "value",
+    "on_horizontalSlider_5_valueChanged",
+    "on_horizontalSlider_rangeChanged",
+    "min",
+    "max",
+    "on_horizontalSlider_valueChanged",
+    "on_horizontalSlider_3_valueChanged",
+    "on_pushButton_clicked",
+    "on_pushButton_2_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      14,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   98,    2, 0x08,    1 /* Private */,
+       3,    0,   99,    2, 0x08,    2 /* Private */,
+       4,    0,  100,    2, 0x08,    3 /* Private */,
+       5,    0,  101,    2, 0x08,    4 /* Private */,
+       6,    1,  102,    2, 0x08,    5 /* Private */,
+       8,    1,  105,    2, 0x08,    7 /* Private */,
+      10,    1,  108,    2, 0x08,    9 /* Private */,
+      11,    1,  111,    2, 0x08,   11 /* Private */,
+      13,    1,  114,    2, 0x08,   13 /* Private */,
+      14,    2,  117,    2, 0x08,   15 /* Private */,
+      17,    1,  122,    2, 0x08,   18 /* Private */,
+      18,    1,  125,    2, 0x08,   20 /* Private */,
+      19,    0,  128,    2, 0x08,   22 /* Private */,
+      20,    0,  129,    2, 0x08,   23 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   15,   16,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_doubleSpinBox_editingFinished'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_doubleSpinBox_2_editingFinished'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_doubleSpinBox_3_editingFinished'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_doubleSpinBox_4_editingFinished'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_comboBox_currentIndexChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_sliderMoved'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_3_sliderMoved'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_4_valueChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_5_valueChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_rangeChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_valueChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_horizontalSlider_3_valueChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_doubleSpinBox_editingFinished(); break;
+        case 1: _t->on_doubleSpinBox_2_editingFinished(); break;
+        case 2: _t->on_doubleSpinBox_3_editingFinished(); break;
+        case 3: _t->on_doubleSpinBox_4_editingFinished(); break;
+        case 4: _t->on_comboBox_currentIndexChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 5: _t->on_horizontalSlider_sliderMoved((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 6: _t->on_horizontalSlider_3_sliderMoved((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 7: _t->on_horizontalSlider_4_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 8: _t->on_horizontalSlider_5_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 9: _t->on_horizontalSlider_rangeChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2]))); break;
+        case 10: _t->on_horizontalSlider_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 11: _t->on_horizontalSlider_3_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 12: _t->on_pushButton_clicked(); break;
+        case 13: _t->on_pushButton_2_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 14)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 14;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 14)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 14;
+    }
+    return _id;
+}
+QT_WARNING_POP
---
+++ mainwindow.h
@@ -0,0 +1,83 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVector>
+#include <QPoint>
+#include <QActionGroup>
+#include <QtDebug>
+#include <QSlider>
+#include <QWheelEvent>
+
+enum Algorithm{
+    STEP_BY_STEP,
+    BRESENHAM,
+    DDA,
+    BRESENHAM_CIRCLE
+};
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_doubleSpinBox_editingFinished();
+
+    void on_doubleSpinBox_2_editingFinished();
+
+    void on_doubleSpinBox_3_editingFinished();
+
+    void on_doubleSpinBox_4_editingFinished();
+
+    void on_comboBox_currentIndexChanged(int index);
+
+    void on_horizontalSlider_sliderMoved(int position);
+
+    void on_horizontalSlider_3_sliderMoved(int position);
+
+    void on_horizontalSlider_4_valueChanged(int value);
+
+    void on_horizontalSlider_5_valueChanged(int value);
+
+    void on_horizontalSlider_rangeChanged(int min, int max);
+
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_horizontalSlider_3_valueChanged(int value);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    void drawSmoothLine(QPainter &painter, int x1, int y1, int x2, int y2);
+    void wheelEvent(QWheelEvent *event);
+    void paintEvent(QPaintEvent*);
+    void swap(double &a,double&b);
+    void recalc();
+    void drawGraph();
+    void checkSpeed();
+
+    int range = 10;
+
+    QPixmap pix;
+    QActionGroup algorithms;
+    Algorithm algorithm;
+
+    QVector<QPoint> StepByStepLine(double x0, double y0, double x1, double y1);
+    QVector<QPoint> BresenhamLine(double x0, double y0, double x1, double y1);
+    QVector<QPoint> DDALine(double x0, double y0, double x1, double y1);
+    QVector<QVector<QPoint>> BresenhamCircle(double x0, double y0,double R);
+    QVector<QSlider*> sliders;
+    void InitializaAlgorithms();
+};
+#endif // MAINWINDOW_H
